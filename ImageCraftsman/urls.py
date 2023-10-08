f3fc788279b173697e444a2cbe76fa3dc8eca263
igor20192 <igor.udovenko2012@meta.ua>
2023-10-08 14:52:57 +0300

    add path __debug__/

--- ImageCraftsman/urls.py
+++ ImageCraftsman/urls.py
@@ -20,7 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = (
-    [path("admin/", admin.site.urls), path("", include("ImageCraftApp.urls"))]
+    [
+        path("admin/", admin.site.urls),
+        path("", include("ImageCraftApp.urls")),
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
