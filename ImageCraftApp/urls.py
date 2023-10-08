665bbce2892efba50bae2880f20c9d31ba3e1f0f
igor20192 <igor.udovenko2012@meta.ua>
2023-10-08 14:53:46 +0300

    add path "serve-image/<int:pk>/"

--- ImageCraftApp/urls.py
+++ ImageCraftApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ImageCreateView, UserDetailView, ImageDetailView
+from .views import ImageCreateView, UserDetailView, ImageDetailView, ServeImageView
 
 urlpatterns = [
     path("upload/", ImageCreateView.as_view(), name="upload-image"),
     path("user/<int:pk>/", UserDetailView.as_view(), name="user-detail"),
     path("image_detail/<int:pk>/", ImageDetailView.as_view(), name="image-detail"),
+    path("serve-image/<int:pk>/", ServeImageView.as_view(), name="serve_image"),
 ]
