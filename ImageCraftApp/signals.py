0285c87fd01b07a6e573c76790335f59e1654231
igor20192 <igor.udovenko2012@meta.ua>
2023-10-20 19:16:01 +0300

    signals.py add create_subscription_plan()

--- ImageCraftApp/signals.py
+++ ImageCraftApp/signals.py
@@ -2,8 +2,42 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import get_object_or_404
 from .models import UserProfile, CustomSubscriptionPlan
+
+
+def create_subscription_plan():
+    """
+    Create default subscription plans.
+
+    This function creates the default subscription plans "Basic," "Premium," and "Enterprise"
+    with their respective settings. If the plans already exist in the database, it does not
+    create duplicate plans.
+
+    Raises:
+        Exception: If an unexpected exception occurs during plan creation.
+    """
+    try:
+        CustomSubscriptionPlan.objects.bulk_create(
+            [
+                CustomSubscriptionPlan(name="Basic", thumbnail_size=200),
+                CustomSubscriptionPlan(
+                    name="Premium",
+                    thumbnail_size=200,
+                    premium_thumbnail_size=400,
+                    original_file=True,
+                ),
+                CustomSubscriptionPlan(
+                    name="Enterprise",
+                    thumbnail_size=200,
+                    premium_thumbnail_size=400,
+                    original_file=True,
+                    expiring_links=True,
+                ),
+            ]
+        )
+    except Exception as e:
+        # Handle any unexpected exceptions during plan creation
+        raise e
 
 
 @receiver(post_save, sender=User)
@@ -25,8 +59,10 @@ def create_user_profile(sender, instance, created, **kwargs):
         Exception: If an unexpected exception occurs during profile creation.
     """
     if created:
+        if not CustomSubscriptionPlan.objects.exists():
+            create_subscription_plan()
         try:
-            subscription_plan = get_object_or_404(CustomSubscriptionPlan, name="Basic")
+            subscription_plan = CustomSubscriptionPlan.objects.get(name="Basic")
             UserProfile.objects.create(
                 user=instance, subscription_plan=subscription_plan
             )
