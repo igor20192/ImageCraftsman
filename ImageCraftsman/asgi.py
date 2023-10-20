693a0750bb9995557e65767f7b36a5cf7606a1e6
igor20192 <igor.udovenko2012@meta.ua>
2023-10-20 15:54:21 +0300

    asgi.py add ASGIStaticFilesHandler

--- ImageCraftsman/asgi.py
+++ ImageCraftsman/asgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ImageCraftsman.settings')
+application = ASGIStaticFilesHandler(get_asgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ImageCraftsman.settings")
 
 application = get_asgi_application()
